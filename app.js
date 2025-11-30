03ea01187db44e4e97a4f6293836c5735efa3304
Ayush Sahu <ayushsahu1430@gmail.com>
2025-11-30 10:13:57 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var name = "Namaste NodeJS";
+
+var a = 2;
+
+var b = 7;
+
+// console.log(name);
+
+// console.log(a + b);
+
+console.log(global);
+
+console.log(globalThis);
+
+// console.log(this); 
