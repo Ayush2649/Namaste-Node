6e85b6566735f8418599b04797bcda946793f50f
Ayush Sahu <ayushsahu1430@gmail.com>
2025-12-02 07:52:43 +0530

    Learned a lot about modules, imports, exports, require, json int Node.js

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-var name = "Namaste NodeJS";
+require("./xyz.js");
+
+const util = require("node: util");
+
+// const {x, calculateSum} = require("./calculate/sum.js");
+
+// const {calculateMultiply} = require("./calculate/multiply");
+
+const {x, calculateSum, calculateMultiply} = require("./calculate");
+
+const data = require("./data.json");
 
 var a = 2;
 
 var b = 7;
 
-// console.log(name);
+calculateSum(a, b);
 
-// console.log(a + b);
+calculateMultiply(a, b);
 
-console.log(global);
-
-console.log(globalThis);
-
-// console.log(this); 
+console.log(data);
---
+++ calculate/sum.js
@@ -0,0 +1,10 @@
+var x = "Hello World";
+
+function calculateSum(a, b) {
+    console.log(a + b);
+}
+
+module.exports = {
+    x: x,
+    calculateSum: calculateSum
+};
